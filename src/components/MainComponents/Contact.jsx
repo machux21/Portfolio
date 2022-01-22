4c10bd951867df7ac1161f36257d3cafd433d870
Francisco Machuca <85341310+machux21@users.noreply.github.com>
2022-01-22 19:16:47 -0300

    form changes

--- src/components/MainComponents/Contact.jsx
+++ src/components/MainComponents/Contact.jsx
@@ -1,14 +1,40 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+const Form = styled.form`
+  width: 60vw;
+  height: 70vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  .name-email {
+    label {
+      text-align: right;
+    }
+    input {
+      width: 100%;
+    }
+  }
+`;
 export default function () {
   return (
     <div>
       <h3>Contact Me</h3>
-      <form>
-        <input type="text" />
-        <input type="email" />
-        <textarea name="message" placeholder="Hola" />
+      <Form>
+        <div className="name-email">
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" />
+        </div>
+        <div className="name-email">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea id="message" name="message" placeholder="Hola" />
+        </div>
+
         <input type="submit" value="submit" />
-      </form>
+      </Form>
     </div>
   );
 }
